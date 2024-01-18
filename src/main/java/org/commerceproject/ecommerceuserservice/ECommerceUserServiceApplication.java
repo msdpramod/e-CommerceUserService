f509322c70ca06273768f92c87c6f98ddef05b89
msdpramod <kuppapramod@gmail.com>
2024-01-18 13:03:49 +0530

    hi usersevice

---
+++ src/main/java/org/commerceproject/ecommerceuserservice/ECommerceUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.commerceproject.ecommerceuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/commerceproject/ecommerceuserservice/ECommerceUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commerceproject.ecommerceuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
